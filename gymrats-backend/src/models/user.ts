61ca88d5d1711a3ef63997e2345027ca270f3f01
JimMartin1810 <martinosdimitrios@gmail.com>
2024-01-09 01:03:00 +0200

    Nothing Done Yet
    Absolutely Nothing

---
+++ gymrats-backend/src/models/user.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  uid: string;
+  name: string;
+  surname: string;
+  address_id: string;
+  email: string;
+  country: string;
+  username: string;
+  password: string;
+}
--- gymrats-backend/src/routes/test.route.ts
+++ gymrats-backend/src/routes/test.route.ts
@@ -4,4 +4,6 @@ import { testFunction } from "../controllers/test.controller";
 const router = express.Router();
 router.route("/").get(testFunction);
 
+router.route("/:id").get(testFunction);
+
 export default router;
