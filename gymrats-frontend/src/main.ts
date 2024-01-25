2423deaf9f4e87bcbcb2dd9cadd3a76f5319dd19
JustDankas <jan-haspel@hotmail.com>
2024-01-26 00:41:58 +0200

    asasdadsdas

--- gymrats-frontend/src/main.ts
+++ gymrats-frontend/src/main.ts
@@ -2,10 +2,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './enviroments/enviroment';
+import { enableProdMode } from '@angular/core';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
 
 if (environment.production) {
+  console.log('Production');
+  enableProdMode();
 }
