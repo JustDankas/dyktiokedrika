bf2c189be3626ebd060029f3f75c09c58508b8b0
orestiskarap <orestiskarapiperis@gmail.com>
2024-02-16 19:51:09 +0200

    added cancel_time_constraint trigger again and modified get announcements to get latest announcements sorted by date and limit count

---
+++ sql/triggers/appointment/cancel_time_constraint.sql
@@ -0,0 +1,11 @@
+CREATE DEFINER = CURRENT_USER TRIGGER cancel_time_constraint
+BEFORE UPDATE ON appointment
+FOR EACH ROW
+BEGIN
+    IF NEW.cancelled = 1 AND OLD.cancelled = 0 THEN
+        IF TIMESTAMPDIFF(HOUR, NOW(), (SELECT start FROM slot WHERE id = NEW.slot_id)) < 2 THEN
+            SIGNAL SQLSTATE '45000' 
+            SET MESSAGE_TEXT = 'Cannot cancel appointment less than 2 hours before start time';
+        END IF;
+    END IF;
+END;
